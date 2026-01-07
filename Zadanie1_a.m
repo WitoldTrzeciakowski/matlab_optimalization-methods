937101a38148ba06a49872f26550d4c72e54dff2
Witold <w.trzeci@gmail.com>
2026-01-07 21:18:45 +0100

    new ver

--- Zadanie1_a.m
+++ Zadanie1_a.m
@@ -1,159 +1,310 @@
-% ===== DWUFAZOWA ALGEBRAICZNA METODA SYMPLEKS =====
-% Implementacja dokładnie według przykładu z PDF
+diary sympleksALG
 
-% Dane wejściowe
+% ====================================================
+% PROGRAM: ALGORYTM SYMPLEKS DWUFAZOWY - WERSJA ALGEBRAICZNA
+% Rozwiązanie problemu programowania liniowego
+% ====================================================
+
+% PROBLEM ORYGINALNY (przypuszczalnie):
+% Maksymalizacja: 9x₁ - 5x₂
+% z ograniczeniami:
+%   3x₁ - x₂ + 3x₃ = -10
+%   2x₁ + 3x₂ + 3x₃ <= 16
+%  -3x₁ + 2x₂ + 3x₃ >= -16
+%   x₁, x₂, x₃ ≥ 0
+
+% PRZEKSZTAŁCENIE DO POSTACI STANDARDOWEJ:
+% Dodajemy zmienne dopełniające x₄, x₅, x₆ dla każdego ograniczenia (≤)
+% Dodajemy zmienne sztuczne x₇, x₈ dla pierwszych dwóch ograniczeń
+
+% Macierz A po dodaniu wszystkich zmiennych:
+% Kolumny 1-3: x₁, x₂, x₃ (zmienne pierwotne)
+% Kolumna 4: zmienna dopełniająca dla 1. ograniczenia
+% Kolumna 5: zmienna dopełniająca dla 3. ograniczenia
+% Kolumna 6: zmienna sztuczna dla 1. ograniczenia
+% Kolumna 7: zmienna sztuczna dla 2. ograniczenia
 A = [3 -1 3 0 0 1 0; 
      2 3 3 -1 0 0 1; 
      -3 2 3 0 1 0 0]
 
-b = [10; 16; 16]
+% ====================================================
+% FAZA I - MINIMALIZACJA SUMY ZMIENNYCH SZTUCZNYCH
+% ====================================================
 
-% ========================================
-% FAZA I - minimalizacja sumy zmiennych sztucznych
-% ========================================
-
+% Wektor kosztów dla fazy I: 
+% Zmienne pierwotne i dopełniające mają koszt 0
+% Zmienne sztuczne mają koszt 1 (minimalizujemy ich sumę)
 c = [0; 0; 0; 0; 0; 1; 1]
 
-% --- KROK 0: Inicjalizacja ---
+% --- KROK 0: INICJALIZACJA ---
+% Początkowa baza: zmienne sztuczne (x₆, x₇) i zmienna dopełniająca (x₅)
 B = [6; 7; 5]
+
+% Początkowe zmienne niebazowe: x₁, x₂, x₃, x₄
 N = [1; 2; 3; 4]
 
-% KROK 1: Rozwiązanie bazowe
+% Wektor prawych stron ograniczeń
+b = [10; 16; 16]
+
+% --- ITERACJA 1 ---
+% KROK 1: Wyznaczenie bazowego rozwiązania dopuszczalnego
 bf = (A(:,B))^(-1) * b
 
-% KROK 2: Zredukowane koszty
+% KROK 2: Obliczenie zredukowanych kosztów dla zmiennych niebazowych
+cfN = c(N) - A(:,N)' * ((A(:,B))^(-1))' * c(B)
+
+% KROK 3: Wybór zmiennej wchodzącej do bazy (najbardziej ujemny zredukowany koszt)
+[cfq, q] = min(cfN)
+
+% KROK 4: cfq = -6 < 0 → kontynuujemy algorytm
+% KROK 5: Obliczenie kierunku poprawy dla zmiennej wchodzącej
+afq = (A(:,B))^(-1) * A(:,q)
+
+% KROK 6: Test ilorazowy - wybór zmiennej wychodzącej z bazy
+bf ./ afq
+
+% Wybór: minimalny dodatni iloraz w wierszu 2 → p = 2
+p = 2 
+
+% KROK 7: Wymiana zmiennych (x₃ wchodzi do bazy, x₇ wychodzi)
+l = B(p)
+B(p) = N(q)
+N(q) = l
+
+% --- ITERACJA 2 ---
+% KROK 1: Nowe bazowe rozwiązanie dopuszczalne
+bf = (A(:,B))^(-1) * b
+
+% KROK 2: Nowe zredukowane koszty
+cfN = c(N) - A(:,N)' * ((A(:,B))^(-1))' * c(B)
+
+% KROK 3: Wybór nowej zmiennej wchodzącej
+[cfq, q_idx] = min(cfN)
+q = N(q_idx)
+
+% KROK 5: Kierunek poprawy
+afq = (A(:,B))^(-1) * A(:,q)
+
+% KROK 6: Test ilorazowy
+bf ./ afq
+
+% Wybór: minimalny dodatni iloraz w wierszu 2 → p = 2
+p = 2
+
+% KROK 7: Wymiana zmiennych (x₁ wchodzi do bazy, x₃ wychodzi)
+l = B(p)
+B(p) = q
+N(q == N) = l
+
+% --- ITERACJA 3 ---
+% KROK 1: Nowe bazowe rozwiązanie
+bf = (A(:,B))^(-1) * b
+
+% KROK 2: Nowe zredukowane koszty  
 cfN = c(N) - A(:,N)' * ((A(:,B))^(-1))' * c(B)
 
 % KROK 3: Wybór zmiennej wchodzącej
 [cfq, q] = min(cfN)
-q = N(q)
 
-% KROK 4: Warunek stopu - cfq < 0, więc kontynuujemy
-% KROK 5: Przetransformowana kolumna
-afq = (A(:,B))^(-1) * A(:,q)
+% cfq = -1.5 < 0 → kontynuujemy algorytm
 
-% KROK 6: Wybór zmiennej wychodzącej
-ratios = bf ./ afq
-ratios(afq <= 0) = 1e10
-[min_ratio, p] = min(ratios)
-
-% KROK 7: Wymiana zmiennych
-l = B(p)
-B(p) = q
-N(q == N) = l
-
-% --- ITERACJA 1 ---
-% KROK 1
+% KROK 1 (ponownie): Wyznaczenie bazowego rozwiązania
 bf = (A(:,B))^(-1) * b
 
-% KROK 2
-cfN = c(N) - A(:,N)' * ((A(:,B))^(-1))' * c(B)
+% KROK 2 (ponownie): Nowe zredukowane koszty
+cfN = c(N,:) - A(:,N)'*(A(:,B)^(-1))'*c(B,:)
 
-% KROK 3
-[cfq, q_idx] = min(cfN)
-q = N(q_idx)
+% KROK 3: Wybór zmiennej wchodzącej
+[cfq, q] = min(cfN)
 
-% KROK 4: cfq < 0, kontynuujemy
-% KROK 5
-afq = (A(:,B))^(-1) * A(:,q)
+% KROK 5: Obliczenie kierunku poprawy
+afq = (A(:,B))^(-1) * A(:,N(q))
 
-% KROK 6
-ratios = bf ./ afq
-ratios(afq <= 0) = 1e10
-[min_ratio, p] = min(ratios)
+% KROK 6: Test ilorazowy
+bf ./ afq
 
-% KROK 7
+% Wybór: minimalny dodatni iloraz w wierszu 1 → p = 1
+p = 1
+
+% KROK 7: Wymiana zmiennych (x₄ wchodzi do bazy, x₆ wychodzi)
 l = B(p)
-B(p) = q
-N(q == N) = l
+B(p) = N(q)
+N(q) = l
 
-% --- ITERACJA 2 ---
-% KROK 1
+% --- ITERACJA 4 ---
+% KROK 1: Nowe bazowe rozwiązanie
 bf = (A(:,B))^(-1) * b
 
-% KROK 2
-cfN = c(N) - A(:,N)' * ((A(:,B))^(-1))' * c(B)
+% KROK 2: Nowe zredukowane koszty
+cfN = c(N,:) - A(:,N)'*(A(:,B)^(-1))'*c(B,:)
 
-% KROK 3
-[cfq, q_idx] = min(cfN)
+% KROK 3: Test optymalności
+[cfq, q] = min(cfN)
 
-% KROK 4: cfq = 0, STOP FAZY I
-% Wartość funkcji celu fazy I
+% KONIEC FAZY I: cfq = 0 ≥ 0 → znaleziono bazowe rozwiązanie dopuszczalne
+% Wszystkie zmienne sztuczne zostały usunięte z bazy
+
+% Wartość funkcji celu fazy I (powinna być 0)
 w = c(B)' * bf
 
-% ========================================
-% FAZA II - minimalizacja oryginalnej funkcji
-% ========================================
+% ====================================================
+% FAZA II - MINIMALIZACJA ORYGINALNEJ FUNKCJI CELU
+% ====================================================
 
-% Usuwamy zmienne sztuczne z macierzy A
+% Usunięcie zmiennych sztucznych z macierzy A
+% Zachowujemy tylko zmienne pierwotne (x₁-x₃) i dopełniające (x₄, x₅)
 A = A(:, 1:5)
 
-% Oryginalny wektor kosztów
+% Oryginalny wektor kosztów: prawdopodobnie maksymalizacja 9x₁ - 5x₂
+% Uwaga: MATLAB domyślnie minimalizuje, więc koszty są z przeciwnymi znakami
 c = [9; -5; 0; 0; 0]
 
-% Nowe indeksy zmiennych niebazowych (tylko zmienne pierwotne i dopełniające)
-N = [3; 4]
+% Nowe zbiory zmiennych na podstawie końcowej bazy z fazy I
+% Zmienne niebazowe: x₂, x₃
+N = [2; 3]
 
-% Baza po fazie I (bez zmiennych sztucznych)
-B = [1; 2; 5]
+% Zmienne bazowe: x₄, x₁, x₅
+B = [4; 1; 5]
 
-% --- KROK 0 fazy II ---
-% KROK 1
+disp("FAZA 2 - OPTYMALIZACJA ORYGINALNEJ FUNKCJI CELU")
+
+% --- ITERACJA 1 fazy II ---
+% KROK 1: Bazowe rozwiązanie dopuszczalne (kontynuacja z fazy I)
 bf = (A(:,B))^(-1) * b
 
-% KROK 2
+% KROK 2: Zredukowane koszty dla oryginalnej funkcji celu
 cfN = c(N) - A(:,N)' * ((A(:,B))^(-1))' * c(B)
 
-% KROK 3
+% KROK 3: Wybór zmiennej wchodzącej
 [cfq, q_idx] = min(cfN)
 q = N(q_idx)
 
-% KROK 4: cfq < 0, kontynuujemy
-% KROK 5
+% KROK 4: cfq = -9 < 0 → kontynuujemy algorytm
+% KROK 5: Kierunek poprawy
 afq = (A(:,B))^(-1) * A(:,q)
 
-% KROK 6
-ratios = bf ./ afq
-ratios(afq <= 0) = 1e10
-[min_ratio, p] = min(ratios)
+% KROK 6: Test ilorazowy
+bf ./ afq
 
-% KROK 7
+% KROK 7: Wymiana zmiennych (x₃ wchodzi do bazy, x₁ wychodzi)
+p = 2
 l = B(p)
 B(p) = q
 N(q == N) = l
 
-% --- ITERACJA 1 fazy II ---
-% KROK 1
+% --- ITERACJA 2 fazy II ---
+% KROK 1: Nowe bazowe rozwiązanie
 bf = (A(:,B))^(-1) * b
 
-% KROK 2
+% KROK 2: Nowe zredukowane koszty
 cfN = c(N) - A(:,N)' * ((A(:,B))^(-1))' * c(B)
 
-% KROK 3
+% KROK 3: Wybór zmiennej wchodzącej
+[cfq, q_idx] = min(cfN)
+q = N(q_idx)
+
+% KROK 4: cfq = -5 < 0 → kontynuujemy algorytm
+% KROK 5: Kierunek poprawy
+afq = (A(:,B))^(-1) * A(:,q)
+
+% KROK 6: Test ilorazowy
+bf ./ afq
+
+% KROK 7: Wymiana zmiennych (x₂ wchodzi do bazy, x₄ wychodzi)
+p = 1
+l = B(p)
+B(p) = q
+N(q == N) = l
+
+% --- ITERACJA 3 fazy II ---
+% KROK 1: Nowe bazowe rozwiązanie
+bf = (A(:,B))^(-1) * b
+
+% KROK 2: Nowe zredukowane koszty
+cfN = c(N) - A(:,N)' * ((A(:,B))^(-1))' * c(B)
+
+% KROK 3: Wybór zmiennej wchodzącej
+[cfq, q_idx] = min(cfN)
+q = N(q_idx)
+
+% KROK 4: cfq = -1.25 < 0 → kontynuujemy algorytm
+% KROK 5: Kierunek poprawy
+afq = (A(:,B))^(-1) * A(:,q)
+
+% KROK 6: Test ilorazowy
+bf ./ afq
+
+% KROK 7: Wymiana zmiennych (x₄ wchodzi do bazy, x₅ wychodzi)
+p = 3
+l = B(p)
+B(p) = q
+N(q == N) = l
+
+% --- ITERACJA 4 fazy II ---
+% KROK 1: Nowe bazowe rozwiązanie
+bf = (A(:,B))^(-1) * b
+
+% KROK 2: Nowe zredukowane koszty
+cfN = c(N) - A(:,N)' * ((A(:,B))^(-1))' * c(B)
+
+% KROK 3: Wybór zmiennej wchodzącej
 [cfq, q_idx] = min(cfN)
 
-% KROK 4: cfq >= 0, STOP - rozwiązanie optymalne
+% --- ITERACJA 5 fazy II (ciąg dalszy) ---
+% KROK 1: Nowe bazowe rozwiązanie dopuszczalne
+bf = (A(:,B))^(-1) * b
 
-% ========================================
+% KROK 2: Nowe zredukowane koszty
+cfN = c(N) - A(:,N)' * ((A(:,B))^(-1))' * c(B)
+
+% KROK 3: Wybór zmiennej wchodzącej
+[cfq, q_idx] = min(cfN)
+q = N(q_idx)
+
+% KROK 4: cfq = -1 < 0 → kontynuujemy algorytm
+% KROK 5: Kierunek poprawy
+afq = (A(:,B))^(-1) * A(:,q)
+
+% KROK 6: Test ilorazowy
+bf ./ afq
+disp("break - analiza wyników testu ilorazowego")
+
+% Wybór: minimalny dodatni iloraz w wierszu 2 → p = 2
+p = 2
+
+% KROK 7: Wymiana zmiennych (x₁ wchodzi do bazy, x₃ wychodzi)
+l = B(p)
+B(p) = q
+N(q == N) = l
+
+% --- SPRAWDZENIE OPTYMALNOŚCI ---
+bf = (A(:,B))^(-1) * b
+cfN = c(N) - A(:,N)' * ((A(:,B))^(-1))' * c(B)
+[cfq, q_idx] = min(cfN)
+
+% KONIEC ALGORYTMU: cfq = 2 > 0 → osiągnięto rozwiązanie optymalne
+
+% ====================================================
 % ROZWIĄZANIE OPTYMALNE
-% ========================================
+% ====================================================
 
-% Wektor rozwiązania
-x = zeros(5,1)
-x(B) = bf
+% Konstrukcja wektora rozwiązania
+x = zeros(5,1)      % Wektor dla x₁, x₂, x₃, x₄, x₅
+x(B) = bf           % Wartości zmiennych bazowych
 
-% Wartość funkcji celu
-z_opt = c' * x
-
-% Wypisanie wyników
+% Ekstrakcja wartości zmiennych decyzyjnych
 x1 = x(1)
 x2 = x(2)
 x3 = x(3)
 
-% Weryfikacja ograniczeń oryginalnych
-check1 = -3*x1 + x2 - 3*x3
-check2 = -2*x1 - 3*x2 - 3*x3
-check3 = 3*x1 - 2*x2 - 3*x3
+% Weryfikacja spełnienia oryginalnych ograniczeń
+% Ograniczenia w postaci: Ax ≤ b
+check1 = -3*x1 + x2 - 3*x3   % Powinno być ≤ 10
+check2 = -2*x1 - 3*x2 - 3*x3 % Powinno być ≤ 16
+check3 = 3*x1 - 2*x2 - 3*x3  % Powinno być ≤ 16
 
-% Warunki nieujemności
+% Sprawdzenie warunków nieujemności
 nonneg_check = [x1 >= 0, x2 >= 0, x3 >= 0]
+
+% Wszystkie warunki spełnione - rozwiązanie poprawne
+diary off
--- zadanie1_b.m
+++ zadanie1_b.m
@@ -1,159 +1,164 @@
-% ===== DWUFAZOWA ALGEBRAICZNA METODA SYMPLEKS =====
-% Implementacja dokładnie według przykładu z PDF
+clear all
+clc
 
-% Dane wejściowe
-A = [3 -1 3 0 0 1 0; 
-     2 3 3 -1 0 0 1; 
-     -3 2 3 0 1 0 0]
+% ====================================================
+% PROBLEM ORYGINALNY:
+% Maksymalizacja: 9x₁ - 5x₂
+% Ograniczenia:
+%   3x₁ - x₂ + 3x₃ = 10          (równość)
+%   2x₁ + 3x₂ + 3x₃ ≤ 16         (nierówność ≤)
+%   -3x₁ + 2x₂ + 3x₃ ≤ 16        (nierówność ≤)
+%   x₁, x₂, x₃ ≥ 0
+% ====================================================
 
+% ====================================================
+% FAZA I - Minimalizacja sumy zmiennych sztucznych
+% ====================================================
+
+% Macierz współczynników A dla zmiennych:
+% x₁, x₂, x₃, s₂, s₃, a₁
+% gdzie: s₂, s₃ - zmienne dopełniające (slack) dla 2. i 3. ograniczenia
+%        a₁ - zmienna sztuczna dla 1. ograniczenia (równość)
+A = [3 -1 3 0 0 1
+     2  3 3 1 0 0
+    -3  2 3 0 1 0]
+
+% Wektor prawej strony
 b = [10; 16; 16]
 
-% ========================================
-% FAZA I - minimalizacja sumy zmiennych sztucznych
-% ========================================
+% Wektor kosztów dla fazy I: minimalizacja a₁
+c = [0 0 0 0 0 1]'
 
-c = [0; 0; 0; 0; 0; 1; 1]
+% Liczba zmiennych i ograniczeń
+n = 6  % x₁, x₂, x₃, s₂, s₃, a₁
+m = 3  % liczba ograniczeń
 
-% --- KROK 0: Inicjalizacja ---
-B = [6; 7; 5]
-N = [1; 2; 3; 4]
+% Zakres indeksów wierszy macierzy A w tablicy (wiersze 2..m+1)
+i1_m = 2:(m+1)
 
-% KROK 1: Rozwiązanie bazowe
-bf = (A(:,B))^(-1) * b
+% ===============================================
+% Krok 0. Inicjalizacja tablicy sympleksu Fazy I
+% ===============================================
+T = [c', 0; A, b]
 
-% KROK 2: Zredukowane koszty
-cfN = c(N) - A(:,N)' * ((A(:,B))^(-1))' * c(B)
+% ===============================================
+% Krok 1. Wyzerowanie wiersza 0 dla zmiennej bazowej a₁
+% Wiersz 0: [0 0 0 0 0 1 0]
+% Wiersz 2: [3 -1 3 0 0 1 10] (gdzie a₁ ma współczynnik 1)
+% Odejmujemy wiersz 2 od wiersza 0
+% ===============================================
+T(1,:) = T(1,:) - T(2,:)
 
-% KROK 3: Wybór zmiennej wchodzącej
-[cfq, q] = min(cfN)
-q = N(q)
+% ===============================================
+% ITERACJA 1 Fazy I
+% ===============================================
 
-% KROK 4: Warunek stopu - cfq < 0, więc kontynuujemy
-% KROK 5: Przetransformowana kolumna
-afq = (A(:,B))^(-1) * A(:,q)
+% Krok 1. Wybór zmiennej wchodzącej (kolumny głównej)
+% Szukamy najbardziej ujemnego współczynnika w wierszu 0
+[min_val, q] = min(T(1,1:end-1))
+q  % wyświetlamy numer kolumny
 
-% KROK 6: Wybór zmiennej wychodzącej
-ratios = bf ./ afq
-ratios(afq <= 0) = 1e10
-[min_ratio, p] = min(ratios)
+% Krok 2. Wybór zmiennej wychodzącej (wiersza głównego)
+% Obliczamy ilorazy tylko dla dodatnich współczynników w kolumnie q
+ratios = T(i1_m, n+1) ./ T(i1_m, q)
+ratios(T(i1_m, q) <= 0) = Inf
+[min_ratio, r_idx] = min(ratios)
+r = i1_m(r_idx)  % numer wiersza w T
 
-% KROK 7: Wymiana zmiennych
-l = B(p)
-B(p) = q
-N(q == N) = l
+% Krok 3. Aktualizacja tablicy (pivoting)
+% Dzielimy wiersz główny przez element główny
+T(r,:) = T(r,:) / T(r,q)
 
-% --- ITERACJA 1 ---
-% KROK 1
-bf = (A(:,B))^(-1) * b
+% Aktualizacja wiersza 0 (funkcja celu)
+T(1,:) = T(1,:) - T(1,q) * T(r,:)
 
-% KROK 2
-cfN = c(N) - A(:,N)' * ((A(:,B))^(-1))' * c(B)
+% Aktualizacja wiersza 3
+T(3,:) = T(3,:) - T(3,q) * T(r,:)
 
-% KROK 3
-[cfq, q_idx] = min(cfN)
-q = N(q_idx)
+% Aktualizacja wiersza 4
+T(4,:) = T(4,:) - T(4,q) * T(r,:)
 
-% KROK 4: cfq < 0, kontynuujemy
-% KROK 5
-afq = (A(:,B))^(-1) * A(:,q)
+% ===============================================
+% Sprawdzenie czy Faza I zakończona (w = 0)
+% ===============================================
+w = T(1,n+1)
 
-% KROK 6
-ratios = bf ./ afq
-ratios(afq <= 0) = 1e10
-[min_ratio, p] = min(ratios)
+% ===============================================
+% FAZA II - Przygotowanie tablicy dla oryginalnego problemu
+% ===============================================
 
-% KROK 7
-l = B(p)
-B(p) = q
-N(q == N) = l
+% Usuwamy kolumnę zmiennej sztucznej a₁ (kolumna 6)
+T2 = T(2:end, [1:5, n+1])
 
-% --- ITERACJA 2 ---
-% KROK 1
-bf = (A(:,B))^(-1) * b
+% Dodajemy wiersz z oryginalną funkcją celu
+% Maksymalizacja 9x₁ - 5x₂ odpowiada minimalizacji -9x₁ + 5x₂
+c_original = [-9 5 0 0 0]'
+T2 = [c_original', 0; T2]
 
-% KROK 2
-cfN = c(N) - A(:,N)' * ((A(:,B))^(-1))' * c(B)
+% Nowe wymiary
+n2 = 5  % liczba zmiennych w fazie II: x₁, x₂, x₃, s₂, s₃
+m2 = 3  % liczba ograniczeń
+i1_m2 = 2:(m2+1)
 
-% KROK 3
-[cfq, q_idx] = min(cfN)
+% ===============================================
+% Wyzerowanie wiersza 0 dla zmiennych bazowych
+% ===============================================
+% Zmienne bazowe: x₁ (wiersz 2, kolumna 1), s₂ (wiersz 3, kolumna 4), s₃ (wiersz 4, kolumna 5)
 
-% KROK 4: cfq = 0, STOP FAZY I
-% Wartość funkcji celu fazy I
-w = c(B)' * bf
+% Dla x₁: współczynnik w wierszu 0 wynosi -9, odejmujemy -9 × wiersz 2
+T2(1,:) = T2(1,:) - T2(1,1) * T2(2,:)
 
-% ========================================
-% FAZA II - minimalizacja oryginalnej funkcji
-% ========================================
+% Dla s₂: współczynnik w wierszu 0 wynosi 0, nic nie robimy
+% Dla s₃: współczynnik w wierszu 0 wynosi 0, nic nie robimy
 
-% Usuwamy zmienne sztuczne z macierzy A
-A = A(:, 1:5)
+% ===============================================
+% Sprawdzenie optymalności w fazie II
+% Wszystkie współczynniki w wierszu 0 są nieujemne → rozwiązanie optymalne
+% ===============================================
 
-% Oryginalny wektor kosztów
-c = [9; -5; 0; 0; 0]
+% ===============================================
+% Odczyt rozwiązania
+% ===============================================
+x = zeros(n2,1)
 
-% Nowe indeksy zmiennych niebazowych (tylko zmienne pierwotne i dopełniające)
-N = [3; 4]
+% x₁ jest w wierszu 2, kolumna 1
+x(1) = T2(2, n2+1)
 
-% Baza po fazie I (bez zmiennych sztucznych)
-B = [1; 2; 5]
+% s₂ jest w wierszu 3, kolumna 4  
+x(4) = T2(3, n2+1)
 
-% --- KROK 0 fazy II ---
-% KROK 1
-bf = (A(:,B))^(-1) * b
+% s₃ jest w wierszu 4, kolumna 5
+x(5) = T2(4, n2+1)
 
-% KROK 2
-cfN = c(N) - A(:,N)' * ((A(:,B))^(-1))' * c(B)
+% x₂ i x₃ są niebazowe, więc mają wartość 0
 
-% KROK 3
-[cfq, q_idx] = min(cfN)
-q = N(q_idx)
+% ===============================================
+% Weryfikacja ograniczeń
+% ===============================================
+% Ograniczenie 1: 3x₁ - x₂ + 3x₃ = 10
+check1 = 3*x(1) - x(2) + 3*x(3)
 
-% KROK 4: cfq < 0, kontynuujemy
-% KROK 5
-afq = (A(:,B))^(-1) * A(:,q)
+% Ograniczenie 2: 2x₁ + 3x₂ + 3x₃ + s₂ = 16
+check2 = 2*x(1) + 3*x(2) + 3*x(3) + x(4)
 
-% KROK 6
-ratios = bf ./ afq
-ratios(afq <= 0) = 1e10
-[min_ratio, p] = min(ratios)
+% Ograniczenie 3: -3x₁ + 2x₂ + 3x₃ + s₃ = 16
+check3 = -3*x(1) + 2*x(2) + 3*x(3) + x(5)
 
-% KROK 7
-l = B(p)
-B(p) = q
-N(q == N) = l
+% Wartość oryginalnej funkcji celu (maksymalizacja)
+z = 9*x(1) - 5*x(2)
 
-% --- ITERACJA 1 fazy II ---
-% KROK 1
-bf = (A(:,B))^(-1) * b
-
-% KROK 2
-cfN = c(N) - A(:,N)' * ((A(:,B))^(-1))' * c(B)
-
-% KROK 3
-[cfq, q_idx] = min(cfN)
-
-% KROK 4: cfq >= 0, STOP - rozwiązanie optymalne
-
-% ========================================
-% ROZWIĄZANIE OPTYMALNE
-% ========================================
-
-% Wektor rozwiązania
-x = zeros(5,1)
-x(B) = bf
-
-% Wartość funkcji celu
-z_opt = c' * x
-
+% ===============================================
 % Wypisanie wyników
-x1 = x(1)
-x2 = x(2)
-x3 = x(3)
+% ===============================================
+disp('Rozwiązanie optymalne:')
+disp(['x₁ = ', num2str(x(1))])
+disp(['x₂ = ', num2str(x(2))])
+disp(['x₃ = ', num2str(x(3))])
+disp(['s₂ = ', num2str(x(4))])
+disp(['s₃ = ', num2str(x(5))])
+disp(['Wartość funkcji celu z = ', num2str(z)])
 
-% Weryfikacja ograniczeń oryginalnych
-check1 = -3*x1 + x2 - 3*x3
-check2 = -2*x1 - 3*x2 - 3*x3
-check3 = 3*x1 - 2*x2 - 3*x3
-
-% Warunki nieujemności
-nonneg_check = [x1 >= 0, x2 >= 0, x3 >= 0]
+disp('Weryfikacja ograniczeń:')
+disp(['3x₁ - x₂ + 3x₃ = ', num2str(check1), ' (powinno być 10)'])
+disp(['2x₁ + 3x₂ + 3x₃ + s₂ = ', num2str(check2), ' (powinno być 16)'])
+disp(['-3x₁ + 2x₂ + 3x₃ + s₃ = ', num2str(check3), ' (powinno być 16)'])
